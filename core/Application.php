d73fd603cdb1a2d40beca026f6cadb2fd01b78d6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-08 08:33:46 +0400

    Add $ROOT_DIR static property to Application.php

--- core/Application.php
+++ core/Application.php
@@ -14,12 +14,14 @@ namespace app\core;
  */
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
     public Response $response;
 
-    public function __construct()
+    public function __construct($rootDir)
     {
+        self::$ROOT_DIR = $rootDir;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 use app\core\Application;
 
 require_once __DIR__.'/../vendor/autoload.php';
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 $app->router->get('/contact', function(){
