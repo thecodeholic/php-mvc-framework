7a1919e3491e19c14ac26bacd0f0dd9dc4303233
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-25 10:09:54 +0400

    Implement session set and session method methods

--- core/Session.php
+++ core/Session.php
@@ -41,6 +41,16 @@ class Session
         return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
     }
 
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return $_SESSION[$key] ?? false;
+    }
+
     public function __destruct()
     {
         $this->removeFlashMessages();
