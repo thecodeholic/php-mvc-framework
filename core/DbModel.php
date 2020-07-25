e9e01dd326b7c2a78b25e3e635340c2f4ee91f71
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-25 09:57:46 +0400

    Create findOne static method in DbModel.php

--- core/DbModel.php
+++ core/DbModel.php
@@ -41,4 +41,17 @@ abstract class DbModel extends Model
     {
         return Application::$app->db->prepare($sql);
     }
+
+    public static function findOne($where)
+    {
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode("AND", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $statement = self::prepare("SELECT * FROM $tableName WHERE $sql");
+        foreach ($where as $key => $item) {
+            $statement->bindValue(":$key", $item);
+        }
+        $statement->execute();
+        return $statement->fetchObject(static::class);
+    }
 }
