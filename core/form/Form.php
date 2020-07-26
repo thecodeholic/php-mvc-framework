dadd3fb00cfe80080f4fb49b0cb643c88203e05d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-26 16:34:28 +0400

    Add attributes support to Form class

--- core/form/Form.php
+++ core/form/Form.php
@@ -18,9 +18,13 @@ use app\core\Model;
  */
 class Form
 {
-    public static function begin($action, $method)
+    public static function begin($action, $method, $options = [])
     {
-        echo sprintf('<form action="%s" method="%s">', $action, $method);
+        $attributes = [];
+        foreach ($options as $key => $value) {
+            $attributes[] = "$key=\"$value\"";
+        }
+        echo sprintf('<form action="%s" method="%s" %s>', $action, $method, implode(" ", $attributes));
         return new Form();
     }
 
