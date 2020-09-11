a08806fa5f3eb202ad55530120acdd05070e163e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-11 08:16:54 +0400

    Add bootstrap.css theme

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="/css/bootstrap.css">
     <title><?php echo $this->title ?></title>
 </head>
 <body>
